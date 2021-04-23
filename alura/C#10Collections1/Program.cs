66e8e1dcf9476e2991bcd03884c4d7bda8009eab
Brenon Ortega <brenon.ortega@nextsoft.com.br>
2021-04-23 13:13:52 -0300

    C# Collections iniciado

---
+++ alura/C#10Collections1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_10Collections1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiraAula = "Olá, está é a primeira aula.";
+            string modelandoAula = "Modelando Arrays na aula!";
+            string ListsAula = "Criando Lists na aula!";
+
+            string[] aulas = new string[]{
+                primeiraAula, modelandoAula, ListsAula
+            };
+
+            foreach (string aula in aulas)
+            {
+                System.Console.WriteLine(aula);    
+            }
+        }
+    }
+}
