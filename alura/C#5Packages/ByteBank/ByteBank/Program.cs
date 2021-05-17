4a3efeebcb55d0197e01c55e1163c434467e1ace
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-05-17 20:12:33 -0300

    Criada solução, projeto  e classlib do ByteBank e refatorado projeto asp.net core

---
+++ alura/C#5Packages/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ByteBank.Modelos.Data;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente Brenon = new("Brenon", 4152156242, "Chefe pae");
+            ContaCorrente conta = new(Brenon, 212, 101, 1);
+
+            System.Console.WriteLine(conta);
+        }
+    }
+}
