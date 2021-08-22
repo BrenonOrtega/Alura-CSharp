d295f84f08472c1d75b48bdabc53e7ba7eda181f
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-08-22 09:07:25 -0300

    Config Startup with Extension Methods.

--- Pluralsight/Identity/Identity.Web/Startup.cs
+++ Pluralsight/Identity/Identity.Web/Startup.cs
@@ -1,39 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Identity.Data;
 using Identity.Data.Repositories;
 using Identity.Domain.Models;
 using Identity.Domain.Repositories;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Identity.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Identity.Data.Extensions;
 
 namespace Identity.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>();
-            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserRepository, DummyUserRepository>();
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(o => o.LoginPath="/Login");
+            var key = Configuration.GetValue<string>("SecretKey");
+            services.SetupJwtAuthentication(key);
 
             services.AddControllersWithViews(o => o.Filters.Add(new AuthorizeFilter()));
         }
@@ -49,9 +47,11 @@ namespace Identity.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios,
+                // see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
