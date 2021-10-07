f9798b2fd841d6b83a27183eb84cc0c5af756842
Brenon Ortega <bortega@acesso.com>
2021-10-07 20:05:44 -0300

    Reorganized folder

---
+++ Pluralsight/EventsAndDelegates/EventsAndDelegates.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventsAndDelegates.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class MyEventRaiser
+    {
+        public event Func<string, string> MyEvent;
+        
+    }
+}
