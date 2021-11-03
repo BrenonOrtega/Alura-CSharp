974aaeb3a2b6ccc9c6e22f4afb9a92ced2f13196
Brenon Ortega <bortega@acesso.com>
2021-11-03 09:24:40 -0300

    Continued Lists/Arrays Course Alura

--- alura/C#7Generics/ByteBank.Modelos/ListaContaCorrentes.cs
+++ alura/C#7Generics/ByteBank.Modelos/ListaContaCorrentes.cs
@@ -5,25 +5,34 @@ namespace ByteBank.Modelos
 {
     public class ListaContaCorrentes
     {
-        
+
         private ContaCorrente[] _contas;
         private int _posicaoDisponivel;
 
-        public ListaContaCorrentes()    
+        public ListaContaCorrentes(int capacidadadeInicial = 10)
         {
-            _contas = new ContaCorrente[5];
-            _posicaoDisponivel = 0;   
+            _contas = new ContaCorrente[capacidadadeInicial];
+            _posicaoDisponivel = 0;
         }
 
-        public void Add(params ContaCorrente[] contas) 
-        { 
-            foreach(var conta in contas)
+        public void Add(params ContaCorrente[] contas)
+        {
+            foreach (var conta in contas)
             {
                 if (IsListFull())
-                    IncreaseSize();
+                    IncreaseSize(_posicaoDisponivel * 2);
 
                 _contas[_posicaoDisponivel++] = conta;
             }
+        }
+
+        public void Remove(ContaCorrente conta)
+        {
+            var indexConta = Array.FindIndex(_contas, x => x.Equals(conta));
+
+            if(indexConta != -1)
+                _contas[indexConta] = null;
+            
         }
 
         public ContaCorrente this[int index]
@@ -35,9 +44,9 @@ namespace ByteBank.Modelos
         public int Count => _contas.Length;
 
         private bool IsListFull() => this.Count >= _posicaoDisponivel;
-        private void IncreaseSize() => Array.Resize(ref _contas, _posicaoDisponivel + 1);
+        private void IncreaseSize(int newCapacity = default) => Array.Resize(ref _contas, newCapacity is default(int) ? _posicaoDisponivel * 2 : newCapacity);
         private void DecreaseSize() => Array.Resize(ref _contas, _posicaoDisponivel - 1);
 
-            
+
     }
 }
