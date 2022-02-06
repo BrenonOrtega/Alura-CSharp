95515c1e743d8deb39cad12f2a828963e9638716
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2022-02-06 16:25:16 -0300

    changes domain models

--- concepts/Outbox.Pattern/Outbox.Pattern.Domain/Billing.cs
+++ concepts/Outbox.Pattern/Outbox.Pattern.Domain/Billing.cs
@@ -4,8 +4,7 @@ namespace Outbox.Pattern.Domain
 {
     public class Billing
     {
-
-        public Billing() {  }
+        public Billing() => Customer = Customer.Empty;
 
         public Billing(Guid id, Customer customer, double amount, double discount)
         {
@@ -14,14 +13,13 @@ namespace Outbox.Pattern.Domain
             Discount = discount;
         }
 
-        public Guid  Id { get; set; }
+        public Guid Id { get; set; }
         public Customer Customer { get; set; } = Customer.Empty;
         public double Amount { get; set; }
         public double Discount { get; set; }
         public double SubTotal { get => Amount; }
         public double Total { get => Amount - Discount; }
         public bool Paid { get; set; }
-        
         public static readonly Billing Empty = new()
         {
             Id = Guid.Empty,
@@ -29,5 +27,44 @@ namespace Outbox.Pattern.Domain
             Discount = -1,
             Paid = false,
         };
+
+        public class Builder
+        {
+            private Billing _billing;
+            public Builder Create(Customer customer) 
+            {
+                _billing = new Billing() { Id = Guid.NewGuid(), Customer = customer };
+                return this;
+            }
+
+            public Builder AddAmount(double amount) 
+            {
+                _billing.Amount += amount;
+
+                return this;
+            }
+
+            public Builder AddDiscount(double value) 
+            {
+                _billing.Discount += value;
+
+                return this;
+            }
+
+            public Builder ChangeCustomer(Customer customer)
+            {
+                _billing.Customer = customer;
+
+                return this;
+            }
+
+            public Billing Build()
+            {
+                var bill = _billing;
+                _billing = default;
+                
+                return bill;
+            }
+        }
     }
 }
--- concepts/Outbox.Pattern/Outbox.Pattern.Domain/Customer.cs
+++ concepts/Outbox.Pattern/Outbox.Pattern.Domain/Customer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Outbox.Pattern.Domain
 {
@@ -7,9 +8,18 @@ namespace Outbox.Pattern.Domain
         public string Name { get; set; }= string.Empty;
         public string Surname { get; set; }= string.Empty;
         public string Document { get; set; }= string.Empty;
-        public IEnumerable<Billing> Bills { get; set; } = new List<Billing>();
+        public IEnumerable<Billing> Bills { get; private set; } = new List<Billing>();
         public static Customer Empty = new();
 
         private Customer() { }
+
+        public Customer(string name, string surname, string document)
+        {
+            Name = name;
+            Surname = surname;
+            Document = document;
+        }
+
+        public void AddBill(Billing bill) => Bills.Append(bill);
     }
 }
